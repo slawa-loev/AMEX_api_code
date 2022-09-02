89fa263efb9ae1a145d0f50f6e5feb7aec18ff3f
Isis Lim <isis.limm@gmail.com>
2022-09-02 12:38:29 +0200

    api using new lightgbm model

--- AMEX_api_code/api/fast.py
+++ AMEX_api_code/api/fast.py
@@ -3,14 +3,37 @@ import pickle
 from pandas import DataFrame
 import json
 import numpy as np
+import lightgbm as lgb
 
 app = FastAPI()
 
 @app.get("/predict")
 def predict(data):
-    model = pickle.load(open('pickles/pp_pred_pipe', 'rb'))
+    model = pickle.load(open('pickles/pp_pred_pipe_gbc_new.pkl', 'rb'))
     param_data = json.loads(data)
     X_pred = DataFrame(param_data,index=[0]).replace('',np.nan)
+
+    def alt_nan_imp(X):
+
+        cat_vars = ['B_30',
+            'B_38',
+            'D_114',
+            'D_116',
+            'D_117',
+            'D_120',
+            'D_126',
+            'D_63',
+            'D_64',
+            'D_66',
+            'D_68']
+
+        alt_nan_list = [-1,-1.0, "-1.0", "-1"]
+
+        cat_columns = [column for column in X.columns if column in cat_vars]
+
+        X[cat_columns] = X[cat_columns].applymap(lambda x: np.nan if x in alt_nan_list else x)
+
+    alt_nan_imp(X_pred)
 
     prediction = model.predict(X_pred)[0]
     pred_probability = model.predict_proba(X_pred)
